69a8d20a7cf13462b7537adeeddb99a5c6763736
Bruno Gonzaga Santos <brunog86@gmail.com>
2024-06-04 23:54:35 -0300

    feat: creating go rotine to complete an auction

--- internal/infra/database/auction/create_auction.go
+++ internal/infra/database/auction/create_auction.go
@@ -2,11 +2,13 @@ package auction
 
 import (
 	"context"
+	"fmt"
 	"fullcycle-auction_go/configuration/logger"
 	"fullcycle-auction_go/internal/entity/auction_entity"
 	"fullcycle-auction_go/internal/internal_error"
-
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"time"
 )
 
 type AuctionEntityMongo struct {
@@ -41,10 +43,36 @@ func (ar *AuctionRepository) CreateAuction(
 		Timestamp:   auctionEntity.Timestamp.Unix(),
 	}
 	_, err := ar.Collection.InsertOne(ctx, auctionEntityMongo)
+
+	go func(ctx context.Context, auctionId string, ar *AuctionRepository) {
+		time.Sleep(30 * time.Second)
+		closeAuctionIfStillOpen(ctx, auctionId, ar)
+	}(ctx, auctionEntity.Id, ar)
+
 	if err != nil {
 		logger.Error("Error trying to insert auction", err)
 		return internal_error.NewInternalServerError("Error trying to insert auction")
 	}
+
+	return nil
+}
+
+func closeAuctionIfStillOpen(ctx context.Context, auctionId string, ar *AuctionRepository) *internal_error.InternalError {
+	fmt.Println("Auction with id", auctionId, "is expired")
+
+	filter := bson.M{"_id": auctionId, "status": auction_entity.Active}
+	update := bson.M{
+		"$set": bson.M{"status": auction_entity.Completed},
+	}
+	result, err := ar.Collection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Error trying to update auction by id = %s", auctionId), err)
+		return internal_error.NewInternalServerError("Error trying to update auction by id")
+	}
+	if result.MatchedCount == 0 {
+		return internal_error.NewInternalServerError("Error trying to update auction by id")
+	}
+	fmt.Println("Auction with id", auctionId, "is expired")
 
 	return nil
 }
